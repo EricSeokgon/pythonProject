b2ffb1963244aa886d232c475d6272e700425275
hadesyi <leesk55@gmail.com>
2020-12-28 21:38:34 +0900

    파이썬으로 csv파일 읽고 쓰기 나만의 모듈 만들어 사용하기

---
+++ chaptor04/usecsv.py
@@ -0,0 +1,16 @@
+import csv, os
+
+
+def opencsv(filename):
+    f = open(filename, 'r')
+    reader = csv.reader(f)
+    output = []
+    for i in reader:
+        output.append(i)
+    return output
+
+
+def writecsv(filename, the_list):
+    with open(filename, 'w', newline='') as f:
+        a = csv.writer(f, delimiter=',')
+        a.writerows(the_list)
